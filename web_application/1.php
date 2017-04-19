7a8c718cc4c17126042869d2578929fac12dbd0c
woobin huh <madgb89@naver.com>
2017-04-19 22:55:35 +0900

    php_basic

---
+++ web_application/1.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>php_practice</title>
+</head>
+<body>
+	<h1>php</h1>
+	<?php
+		echo "Hello World";
+		echo 10+10;
+		echo "10+10";
+		echo "10"."10";
+	?>
+	<h1>javascript</h1>
+	<script>
+		document.write("Hello world");
+	</script>
+</body>
+</html>
