2b5411f948dcd58fbad5acb0cc174f7d4a632ea6
woobin huh <madgb89@naver.com>
2017-04-25 21:11:11 +0900

    yo

--- web_application/1.php
+++ web_application/1.php
@@ -12,9 +12,18 @@
 		echo "10+10";
 		echo "10"."10";
 	?>
+	<h2>php2</h2>
+	<?php
+		$name = "madgb";
+		echo "hello"." "."I'm"." ".$name;
+		var_dump(1==1);
+		var_export(1==1);
+	?>
 	<h1>javascript</h1>
 	<script>
 		document.write("Hello world");
+		var name = "madgb";
+		document.write("hello "+"I'm "+name);
 	</script>
 </body>
 </html>
